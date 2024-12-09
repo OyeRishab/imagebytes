8f4111d02839d690f846e9c25118b9df351182c2
Rishab Jain <85238177+OyeRishab@users.noreply.github.com>
2024-12-10 00:50:18 +0530

    Update train.py

--- train.py
+++ train.py
@@ -12,35 +12,35 @@ LR = 1e-4
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 print(DEVICE)
 
-# Load Data
-# dataset = SAROpticalDataset('images/SAR1/', 'images/OI1/')
-# dataloader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True)
 
-# # Initialize Model, Loss, Optimizer
-# model = ColorizationUNet().to(DEVICE)
-# criterion = nn.MSELoss()
-# optimizer = optim.Adam(model.parameters(), lr=LR)
+dataset = SAROpticalDataset('images/SAR1/', 'images/OI1/')
+dataloader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True)
 
-# # Training Loop
-# for epoch in range(EPOCHS):
-#     model.train()
-#     total_loss = 0
+# Initialize Model, Loss, Optimizer
+model = ColorizationUNet().to(DEVICE)
+criterion = nn.MSELoss()
+optimizer = optim.Adam(model.parameters(), lr=LR)
 
-#     for sar, optical in dataloader:
-#         sar, optical = sar.float().to(DEVICE), optical.float().to(DEVICE)
+# Training Loop
+for epoch in range(EPOCHS):
+    model.train()
+    total_loss = 0
 
-#         # Forward pass
-#         output = model(sar)
-#         loss = criterion(output, optical)
+    for sar, optical in dataloader:
+        sar, optical = sar.float().to(DEVICE), optical.float().to(DEVICE)
+
+        # Forward pass
+        output = model(sar)
+        loss = criterion(output, optical)
         
-#         # Backward pass
-#         optimizer.zero_grad()
-#         loss.backward()
-#         optimizer.step()
+        # Backward pass
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
 
-#         total_loss += loss.item()
+        total_loss += loss.item()
 
-#     print(f"Epoch [{epoch+1}/{EPOCHS}], Loss: {total_loss/len(dataloader):.4f}")
+    print(f"Epoch [{epoch+1}/{EPOCHS}], Loss: {total_loss/len(dataloader):.4f}")
 
-# # Save the trained model
-# torch.save(model.state_dict(), "sar_colorization.pth")
+# Save the trained model
+torch.save(model.state_dict(), "sar_colorization.pth")
