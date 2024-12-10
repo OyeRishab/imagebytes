16a151d3365e4d3bce1a26be497c416d14db76e0
Rishab Jain <85238177+OyeRishab@users.noreply.github.com>
2024-12-11 00:34:48 +0530

    Update test.py

--- test.py
+++ test.py
@@ -7,15 +7,18 @@ import numpy as np
 import os
 
 # Visualization Function
-def visualize_output(sar, output, idx):
+def visualize_output(sar, optical, output, idx):
     sar = sar.cpu().numpy().squeeze()
+    optical = optical.cpu().numpy().squeeze().transpose(1, 2, 0)
     output = output.cpu().numpy().squeeze().transpose(1, 2, 0)
 
-    fig, ax = plt.subplots(1, 2, figsize=(10, 5))
+    fig, ax = plt.subplots(1, 3, figsize=(15, 5))
     ax[0].imshow(sar, cmap='gray')
     ax[0].set_title(f'SAR Image {idx}')
-    ax[1].imshow(output)
-    ax[1].set_title(f'Colorized Output {idx}')
+    ax[1].imshow(optical)
+    ax[1].set_title(f'Optical Image {idx}')
+    ax[2].imshow(output)
+    ax[2].set_title(f'Colorized Output {idx}')
     
     # Save the figure
     if not os.path.exists('output_images'):
@@ -49,7 +52,7 @@ def test_model():
             output = model(sar, sar)
 
             # Visualize output
-            visualize_output(sar[0], output[0], idx)
+            visualize_output(sar[0], optical[0], output[0], idx)
 
 # Run the testing function
 test_model()
