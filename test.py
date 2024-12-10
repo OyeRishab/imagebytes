78907e0fe8caaa49c531576fd16bb0311167bda1
Rishab Jain <85238177+OyeRishab@users.noreply.github.com>
2024-12-11 01:23:58 +0530

    output

--- test.py
+++ test.py
@@ -1,4 +1,3 @@
-import torch
 from torch.utils.data import DataLoader
 from utils import SAROpticalDataset
 from model import SPADEGenerator
@@ -13,49 +12,26 @@ def visualize_output(sar, optical, output, idx):
     output = output.cpu().numpy().squeeze().transpose(1, 2, 0)
 
     # Denormalize from [-1, 1] to [0, 1]
-    optical = (optical + 1) / 2
-    output = (output + 1) / 2
+    optical = (optical + 1) / 2.0
+    output = (output + 1) / 2.0
+
+    # Clip values to [0, 1]
+    optical = np.clip(optical, 0, 1)
+    output = np.clip(output, 0, 1)
 
     fig, ax = plt.subplots(1, 3, figsize=(15, 5))
     ax[0].imshow(sar, cmap='gray')
     ax[0].set_title(f'SAR Image {idx}')
-    ax[1].imshow(optical.clip(0, 1))
+    ax[0].axis('off')
+    ax[1].imshow(optical)
     ax[1].set_title(f'Optical Image {idx}')
-    ax[2].imshow(output.clip(0, 1))
+    ax[1].axis('off')
+    ax[2].imshow(output)
     ax[2].set_title(f'Colorized Output {idx}')
-    
+    ax[2].axis('off')
+
+    # Save the figure
     if not os.path.exists('output_images'):
         os.makedirs('output_images')
-    plt.savefig(f'output_images/output_{idx}.png')
+    plt.savefig(f'output_images/output_{idx}.png', bbox_inches='tight')
     plt.close(fig)
-
-# Testing Function
-def test_model():
-    # Parameters
-    BATCH_SIZE = 16
-    DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
-    MODEL_PATH = "sar_colorization_spade.pth"
-
-    # Load test dataset
-    test_dataset = SAROpticalDataset('images/sar_test/', 'images/oi_test/')
-    test_dataloader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=False)
-
-    # Initialize Model
-    model = SPADEGenerator().to(DEVICE)
-    model.load_state_dict(torch.load(MODEL_PATH))
-    model.eval()
-
-    # Testing Loop
-    with torch.no_grad():
-        for idx, (sar, optical) in enumerate(test_dataloader):
-            sar = sar.float().to(DEVICE)
-            optical = optical.float().to(DEVICE)
-
-            # Forward pass
-            output = model(sar, sar)
-
-            # Visualize output
-            visualize_output(sar[0], optical[0], output[0], idx)
-
-# Run the testing function
-test_model()
