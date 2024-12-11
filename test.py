fe5cdead7b30bc48da90d849fc7bfb19357997c9
Rishab Jain <85238177+OyeRishab@users.noreply.github.com>
2024-12-11 13:18:27 +0530

    cnn code

--- test.py
+++ test.py
@@ -1,62 +1,37 @@
-import torch
 from torch.utils.data import DataLoader
 from utils import SAROpticalDataset
 from model import SPADEGenerator
-import os
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
+import os
 
-# Function to save generated images
-def save_generated_image(sar, gen_optical, idx):
+# Visualization Function
+def visualize_output(sar, optical, output, idx):
     sar = sar.cpu().numpy().squeeze()
-    gen_optical = gen_optical.cpu().numpy().squeeze().transpose(1, 2, 0)
+    optical = optical.cpu().numpy().squeeze().transpose(1, 2, 0)
+    output = output.cpu().numpy().squeeze().transpose(1, 2, 0)
 
     # Denormalize from [-1, 1] to [0, 1]
-    gen_optical = (gen_optical + 1) / 2.0
+    optical = (optical + 1) / 2.0
+    output = (output + 1) / 2.0
 
     # Clip values to [0, 1]
-    gen_optical = np.clip(gen_optical, 0, 1)
+    optical = np.clip(optical, 0, 1)
+    output = np.clip(output, 0, 1)
 
-    fig, ax = plt.subplots(1, 2, figsize=(10, 5))
+    fig, ax = plt.subplots(1, 3, figsize=(15, 5))
     ax[0].imshow(sar, cmap='gray')
     ax[0].set_title(f'SAR Image {idx}')
     ax[0].axis('off')
-    ax[1].imshow(gen_optical)
-    ax[1].set_title(f'Generated Optical Image {idx}')
+    ax[1].imshow(optical)
+    ax[1].set_title(f'Optical Image {idx}')
     ax[1].axis('off')
+    ax[2].imshow(output)
+    ax[2].set_title(f'Colorized Output {idx}')
+    ax[2].axis('off')
 
     # Save the figure
-    if not os.path.exists('generated_images'):
-        os.makedirs('generated_images')
-    plt.savefig(f'generated_images/generated_{idx}.png', bbox_inches='tight')
+    if not os.path.exists('output_images'):
+        os.makedirs('output_images')
+    plt.savefig(f'output_images/output_{idx}.png', bbox_inches='tight')
     plt.close(fig)
-
-# Testing Function
-def test_model():
-    # Parameters
-    BATCH_SIZE = 16
-    DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
-    MODEL_PATH = "sar_colorization_gan.pth"  # Path to the trained GAN generator model
-
-    # Load test dataset (SAR images only)
-    test_dataset = SAROpticalDataset('images/sar_test/', 'images/oi_test/')
-    test_dataloader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=False)
-
-    # Initialize Generator
-    generator = SPADEGenerator().to(DEVICE)
-    generator.load_state_dict(torch.load(MODEL_PATH))
-    generator.eval()
-
-    # Generate new images
-    with torch.no_grad():
-        for idx, (sar, _) in enumerate(test_dataloader):
-            sar = sar.to(DEVICE)
-            # Generate images
-            gen_optical = generator(sar, sar)
-            # Save generated images
-            batch_size = sar.size(0)
-            for i in range(batch_size):
-                save_generated_image(sar[i], gen_optical[i], idx * BATCH_SIZE + i)
-
-# Run the testing function
-test_model()
--- train.py
+++ train.py
@@ -3,82 +3,47 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
 from utils import SAROpticalDataset
-from model import SPADEGenerator, Discriminator, UNet
+from model import SPADEGenerator
 
 # Parameters
 BATCH_SIZE = 16
 EPOCHS = 100
 LR = 1e-4
-GR = 2e-1
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+print(f"Using device: {DEVICE}")
 
 # Load Data
 dataset = SAROpticalDataset('images/sar_train/', 'images/oi_train/')
 dataloader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True)
+print(f"Number of training samples: {len(dataset)}")
+print(f"Number of batches: {len(dataloader)}")
 
-# Initialize Models
-generator = SPADEGenerator().to(DEVICE)
-discriminator = Discriminator().to(DEVICE)
-unet = UNet(in_channels=1, out_channels=3).to(DEVICE)
-
-# Loss Functions
-adversarial_loss = nn.BCEWithLogitsLoss()
-pixelwise_loss = nn.L1Loss()
-
-# Optimizers
-optimizer_G = optim.Adam(generator.parameters(), lr=GR)
-optimizer_D = optim.Adam(discriminator.parameters(), lr=LR)
-optimizer_U = optim.Adam(unet.parameters(), lr=LR)
+# Initialize Model, Loss, Optimizer
+model = SPADEGenerator().to(DEVICE)
+criterion = nn.MSELoss()
+optimizer = optim.Adam(model.parameters(), lr=LR)
 
 # Training Loop
 for epoch in range(EPOCHS):
+    model.train()
+    total_loss = 0
+
     for sar, optical in dataloader:
-        sar = sar.to(DEVICE)
-        optical = optical.to(DEVICE)
+        sar = sar.float().to(DEVICE)
+        optical = optical.float().to(DEVICE)
 
-        # Generate images
-        gen_optical = generator(sar, sar)
+        # Forward pass
+        optimizer.zero_grad()
+        output = model(sar, sar)
+        loss = criterion(output, optical)
 
-        # ---------------------
-        #  Train Discriminator
-        # ---------------------
-        features = unet(sar)
-        optimizer_D.zero_grad()
+        # Backward pass
+        loss.backward()
+        optimizer.step()
 
-        # Real images
-        real_input = torch.cat((sar, optical), 1)
-        pred_real = discriminator(real_input)
-        output_size = pred_real.size()[2:]
-        valid = torch.ones(sar.size(0), 1, *output_size).to(DEVICE)
+        total_loss += loss.item()
 
-        # Fake images
-        fake_input = torch.cat((sar, gen_optical.detach()), 1)
-        pred_fake = discriminator(fake_input)
-        fake = torch.zeros(sar.size(0), 1, *output_size).to(DEVICE)
+    print(f"Epoch [{epoch+1}/{EPOCHS}], Loss: {total_loss/len(dataloader):.4f}")
 
-        # Total loss
-        loss_real = adversarial_loss(pred_real, valid)
-        loss_fake = adversarial_loss(pred_fake, fake)
-        loss_D = (loss_real + loss_fake) * 0.5
-
-        loss_D.backward()
-        optimizer_D.step()
-
-        # ---------------------
-        #  Train Generator
-        # ---------------------
-        optimizer_G.zero_grad()
-
-        # Loss measures generator's ability to fool the discriminator
-        pred_fake = discriminator(fake_input)
-        loss_GAN = adversarial_loss(pred_fake, valid)
-        loss_pixel = pixelwise_loss(gen_optical, optical)
-        loss_G = loss_GAN + 100 * loss_pixel
-
-        loss_G.backward()
-        optimizer_G.step()
-
-    print(f"Epoch [{epoch+1}/{EPOCHS}] Loss D: {loss_D.item():.4f}, Loss G: {loss_G.item():.4f}")
-
-# Save the generator model
-torch.save(generator.state_dict(), "sar_colorization_gan.pth")
+# Save the trained model
+torch.save(model.state_dict(), "sar_colorization_spade.pth")
